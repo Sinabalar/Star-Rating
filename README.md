b32f15fd66f53ab8f5be6e2635ca8ef97234ac03
Sinabalar <sinabalar666@gmail.com>
2024-09-29 18:42:19 +0330

    feat: Enhance StarRating component
    - Add customizable color, size, and className props
    - Implement defaultRating and onRate props for better control
    - Display messages based on rating and tempRating
    - Update Star component to use dynamic color and size

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ This is a simple and customizable star rating component built with React.
 ## Features
 
 - Customizable maximum rating (default is 5 stars)
+- Customizable star color and size
 - Hover and click interactions for rating
 - Displays filled and unfilled star icons based on rating
 - Inline styling for easy customization
+- Optional messages to display based on rating
+- Supports default rating and onRate callback
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <StarRating maxRating={5}/>
-  </React.StrictMode>
+    <React.StrictMode>
+        <StarRating maxRating={5}
+                    messages={['Terrible','Bad','Okay','Good','Amazing']}
+                    defaultRating={3}
+        />
+        {/*<Test/>*/}
+    </React.StrictMode>
 );
+
+// for test the onRate prop
+
+/*
+function Test(){
+    const [rated,setRated]=useState(0)
+
+    return(
+        <div>
+        <StarRating onRate={setRated}/>
+            <p>Was rated by {rated} stars</p>
+        </div>
+    )
+}
+*/
